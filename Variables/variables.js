18ae09de12e09db85e8622dc67d116d12d9e5ddf
shivmundhe7 <shivmundhe16@gmail.com>
2025-07-16 16:47:43 +0530

    js

---
+++ Variables/variables.js
@@ -0,0 +1,19 @@
+let a = 34;
+let b = 45;
+let c = 3.55;
+const p =true;
+let q = undefined;
+let r = null
+
+console.log(a,b,c,p,q,r)
+
+console.log(typeof a, typeof b, typeof c, typeof p )
+
+let o = {
+    name :"Shiv",
+    "job code" : 5000,
+    is_handsome: true
+}
+
+console.log(o)
+o.salary = "100crores";
