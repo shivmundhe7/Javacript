7c8dd5054eeda003d57707631ba5efc7dfbaa6af
shivmundhe7 <shivmundhe16@gmail.com>
2025-07-22 00:15:32 +0530

    DOM

--- Chapter_5/array.js
+++ Chapter_5/array.js
@@ -17,30 +17,40 @@
 //         const element = object[key];
 //     }
 // }
-let arr = [1, 13, 5 ,7, 11];
+const numbers = [1,2,3,4]
+numbers.splice(2,0,35,76)
+console.log(numbers)
+
+// Create an Array
+// const num = ["Banana", "Orange", "Apple", "Mango"];
+
+// // At position 2, add "Lemon" and "Kiwi":
+// num.splice(2, 0, "Lemon", "Kiwi");
+// console.log(num)
+// let arr = [1, 13, 5 ,7,11];
 // let newArr = []
 // for (let index = 0; index < arr.length; index++) {
 //     const element = arr[index];
 //     newArr.push(element**2)
 // }
 
-let newArr = arr.map((e, index, array)=>{
-    return e**2
-})
+// let newArr = arr.map((e, index, array)=>{
+//     return e**2
+// })
 
-console.log(newArr)
-const greaterThanSeven = (e)=>{
-    if(e>7){
-        return true
-    }
-    return false
-}
-console.log(arr.filter(greaterThanSeven))
+// console.log(newArr)
+// const greaterThanSeven = (e)=>{
+//     if(e>7){
+//         return true
+//     }
+//     return false
+// }
+// console.log(arr.filter(greaterThanSeven))
 
-let arr2 = [1,2,3,4,5,6]
+// let arr2 = [1,2,3,4,5,6]
 
-const red = (a, b)=>{
-    return a+b
-}
+// const red = (a, b)=>{
+//     return a+b
+// }
 
-console.log(arr2.reduce(red))
+// console.log(arr2.reduce(red))
--- Chapter_6/Dom.js
+++ Chapter_6/Dom.js
@@ -1,1 +1,7 @@
 console.log("Hello World")
+// let bhupendrajogis = document.getElementsByClassName("box")
+// console.log(bhupendrajogis) 
+
+// bhupendrajogis[2].style.backgroundColor="red"
+
+document.getElementById("jogi").style.backgroundColor = "pink"
