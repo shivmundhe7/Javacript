8918d1188ed06d7e63390d0f4b126e44d33fc8a8
shivmundhe7 <shivmundhe16@gmail.com>
2025-07-24 01:22:12 +0530

    JS

---
+++ Chapter_8/events/index.js
@@ -0,0 +1,10 @@
+// let button = document.getElementById("btn")
+
+// button.addEventListener("click",()=> {
+//     document.querySelector(".box").innerHTML = "<b> Yayy!!! you were Clicked</b> Enjoy Your Click"
+// })
+
+let button = document.getElementById("btn")
+button.addEventListener("click",()=> {
+    document.querySelector(".box").innerHTML = "<b>Yaay! You were Clicked </b> Enjoy Your Click" 
+})
